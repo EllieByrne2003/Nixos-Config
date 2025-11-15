a64cbe9628108cfe1eb3ad47148968f3cb2ef071
EllieByrne2003 <leonbyrne52@gmail.com>
2025-11-15 23:46:10 +0000

    nixpkgs wass the culprit.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
     description = "Ellie Byrne's Nixos configuration";
 
     inputs = {
-        nixpkgs.url = "github/:NixOS/nixpkgs/nixos-25.05";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
         home-manager = {
             url = "github:nix-community/home-manager/release-25.05";
