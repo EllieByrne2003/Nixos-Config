5bb3c88d212a8a55ec29d8f27ab39d3b275256af
EllieByrne2003 <leonbyrne52@gmail.com>
2025-11-16 14:49:51 +0000

    oops.

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -22,7 +22,7 @@
     programs.hyprland = {
         enable = true;
 
-        package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+        # package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     };
 
     programs.hyprlock.enable = true;
