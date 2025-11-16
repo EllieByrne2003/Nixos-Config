72dd267025dc55d7f5840ee239381875ebc54d67
EllieByrne2003 <leonbyrne52@gmail.com>
2025-11-16 14:33:22 +0000

    Testing without hypridle.

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -131,31 +131,31 @@
 		};
 	};
 
-    services.hypridle = {
-        enable = true;
+    # services.hypridle = {
+    #     enable = true;
 
-        settings = {
-            general = {
-                after_sleep_cmd = "hyprctl dispatch dpms on";
-                ignore_dbus_inhibit = false;
-                lock_cmd = "hyprlock";
-            };
+    #     settings = {
+    #         general = {
+    #             after_sleep_cmd = "hyprctl dispatch dpms on";
+    #             ignore_dbus_inhibit = false;
+    #             lock_cmd = "hyprlock";
+    #         };
 
-            listener = [
-                {
-                    timeout = 300; # 5 minutes
-                    on-timeout = "hyprlock";
-                }
-                {
-                    timeout = 360; # 6 minutes
-                    on-timeout = "hyprctl dispatch dpms off";
-                    on-resume = "hyprctl dispatch dpms on";
-                }
-                {
-                    timeout = 600; # 10 minutes
-                    on-timeout = "systemctl suspend";
-                }
-            ];
-        };
-    };
+    #         listener = [
+    #             {
+    #                 timeout = 300; # 5 minutes
+    #                 on-timeout = "hyprlock";
+    #             }
+    #             {
+    #                 timeout = 360; # 6 minutes
+    #                 on-timeout = "hyprctl dispatch dpms off";
+    #                 on-resume = "hyprctl dispatch dpms on";
+    #             }
+    #             {
+    #                 timeout = 600; # 10 minutes
+    #                 on-timeout = "systemctl suspend";
+    #             }
+    #         ];
+    #     };
+    # };
 }
